25cf76dc24485fe84c34cab82c99d957447feca0
Samuel Rivero <jsamiju13@gmail.com>
2024-09-20 12:06:17 -0400

    v1

---
+++ functions.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll('.hidden-element');
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    });
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
+
+let correo = document.querySelector(".correo")
+
+let botonContacto = document.getElementById("botonContacto").addEventListener("click", function(){
+    Swal.fire({
+        title: "Contacto",
+        html: correo,
+        icon: "info"
+    });
+})
+
+document.getElementById('toggleButton').addEventListener('click', function() {
+    var menu = document.getElementById('dropdownMenu');
+    menu.classList.toggle('hidden');
+});
