dced86aa250bb56630e9ebbbaf8c695c64229568
Samuel Rivero <jsamiju13@gmail.com>
2024-10-07 19:54:06 -0400

    atoi go

---
+++ go/act3/atoi.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+func clearConsole() {
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func main() {
+
+	fmt.Println("**********SUPER CONVERSOR DE UNIDADES**********")
+
+	lectura := bufio.NewScanner(os.Stdin)
+	fmt.Println("Ingrese Kilometros, Kilogramos y Horas:")
+	lectura.Scan()
+	datos := strings.Split(lectura.Text(), " ")
+
+	kmStr := datos[0]
+	kgStr := datos[1]
+	hourStr := datos[2]
+
+	km, _ := strconv.Atoi(kmStr)
+	miles := float64(km) * 0.621371
+
+	kg, _ := strconv.Atoi(kgStr)
+	pounds := float64(kg) * 2.20462
+
+	hour, _ := strconv.Atoi(hourStr)
+	seconds := float64(hour) * 3600
+
+	clearConsole()
+	fmt.Println("**********SUPER CONVERSOR DE UNIDADES**********")
+	fmt.Printf("%d kilómetros son %.2f millas.\n", km, miles)
+	fmt.Printf("%d kilogramos son %.3f libras.\n", kg, pounds)
+	fmt.Printf("%d horas son %.2f segundos.\n", hour, seconds)
+
+	fmt.Println("Presiona Enter para salir...")
+	fmt.Scanln()
+}
